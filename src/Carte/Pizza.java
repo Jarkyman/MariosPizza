46c8cd93f1a3925b129a52a0b6d5cca1490a07c5
Christoffer <christofferp97@hotmail.com>
2021-04-19 14:10:13 +0200

    Pizza class added constructor

--- src/Carte/Pizza.java
+++ src/Carte/Pizza.java
@@ -1,4 +1,17 @@
 package Carte;
 
+import java.util.ArrayList;
+
 public class Pizza {
+
+
+  private String name;
+  private ArrayList<String>toppings = new ArrayList<>();
+  private double price;
+
+  public Pizza(String name, ArrayList<String> toppings, double price) {
+    this.name = name;
+    this.toppings = toppings;
+    this.price = price;
+  }
 }
