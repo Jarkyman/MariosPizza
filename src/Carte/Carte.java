6150727908095b228d5d2f543ae3cf9fc0592189
Jackie <Jack3459@stud.kea.dk>
2021-04-19 14:08:29 +0200

    Rod i carte

--- src/Carte/Carte.java
+++ src/Carte/Carte.java
@@ -1,2 +1,21 @@
-package Carte;public class Carte {
+package Carte;
+
+import java.util.ArrayList;
+
+public class Carte {
+
+  public ArrayList<String> topping = new ArrayList<String>();
+
+  public void createAllPizza() {
+
+    topping.add("Ost");
+    topping.add("Tomat");
+    new Pizza ("Hej", topping, 57);
+    topping.add("Kebab");
+    new Pizza ("Pizza 2", topping, 89);
+    topping.remove(2);
+    topping.add("Peperoni");
+    new Pizza("Pizza 3", topping, 58);
+  }
+
 }
