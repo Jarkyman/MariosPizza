2725f46f92f0efc21b1682c1c91eff45d7a49db8
jacki <jack3459@stud.kea.dk>
2021-04-20 10:50:33 +0200

    Add RegisterMenu.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-import carte.Carte;
-import carte.Pizza;
 import menu.MainMenu;
-import ui.FileHandling;
-
-import java.util.ArrayList;
 
 public class Main {
 
-
   public static void main(String[] args) throws InterruptedException {
-
     new MainMenu().runMainMenu();
-
-
   }
 
 }
--- src/menu/RegisterMenu.java
+++ src/menu/RegisterMenu.java
@@ -1,34 +1,55 @@
 package menu;
 
+import carte.Pizza;
 import ui.UI;
 
+import java.util.ArrayList;
+
 public class RegisterMenu {
-  UI ui = new UI();
-  MainMenu mainMenu = new MainMenu();
+  private UI ui = new UI();
+  private MainMenu mainMenu = new MainMenu();
+  private ArrayList<Pizza> pizzaInOrder = new ArrayList<>();
 
-  public void runRegisterMenu(){
-    int choice = 0;
 
-    while(choice != 9){
-      ui.printRegisterMenu();
-      choice = ui.readChoice();
-
-      switch(choice){
-        case 1:
-          choosePizza();
-          break;
-        case 2:
-          chooseExtraTopping();
-          break;
-        case 3:
-          addExtraPizza();
-          break;
-        case 4:
-          finishOrder();
-          break;
-        case 9:
-          mainMenu.runMainMenu();
-      }
+  public void registerPizza(ArrayList<Pizza> pizzasInMenu) {
+    while (!morePizza()) {
+      pizzaInOrder.add(pizzaInOrder(choosePizza(pizzasInMenu)));
+      morePizza();
     }
+
   }
+
+  private boolean morePizza() {
+
+    UI ui = new UI("Do you want more?");
+    int choice = ui.readChoice();
+
+    if (choice == 1) {
+      return false;
+    } else if (choice == 2) {
+      return true;
+    }
+
+    return true;
+  }
+
+  public Pizza choosePizza(ArrayList<Pizza> pizzaInMenu) {
+
+    UI ui = new UI(pizzaInMenu, "Choose Pizza: ");
+
+    Pizza pizza = pizzaInMenu.get(ui.readChoice() - 1);
+
+    //chooseToping();
+
+    return pizza;
+  }
+
+  private void chooseToping() {
+
+    // tilføj topping
+
+    // + ekstra pris
+
+  }
+
 }
