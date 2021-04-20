d5363949d527166775d0c1d8088680aa1145fc1f
jacki <jack3459@stud.kea.dk>
2021-04-20 09:02:53 +0200

    Prøver at komme med

--- src/carte/Pizza.java
+++ src/carte/Pizza.java
@@ -4,35 +4,33 @@ import java.util.ArrayList;
 
 public class Pizza {
 
-    private String name;
-    private ArrayList<String>toppings = new ArrayList<>();
-    private double price;
+  private String name;
+  private ArrayList<String>toppings = new ArrayList<>();
+  private double price;
 
-    public Pizza(String name, ArrayList<String> toppings, double price) {
-      this.name = name;
-      this.toppings = toppings;
-      this.price = price;
-    }
-
-    public String getName() {
-      return name;
-    }
-    public void setName(String name) {
-      this.name = name;
-    }
-    public ArrayList<String>getToppings() {
-      return toppings;
-    }
-    public void setToppings(ArrayList<String> toppings) {
-      this.toppings = toppings;
-    }
-    public double getPrice()  {
-      return price;
-    }
-    public void setPrice(double price)  {
-
-      this.price = price;
-    }
+  public Pizza(String name, ArrayList<String> toppings, double price) {
+    this.name = name;
+    this.toppings = toppings;
+    this.price = price;
   }
 
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public ArrayList<String>getToppings() {
+    return toppings;
+  }
+  public void setToppings(ArrayList<String> toppings) {
+    this.toppings = toppings;
+  }
+  public double getPrice()  {
+    return price;
+  }
+  public void setPrice(double price)  {
 
+    this.price = price;
+  }
+}
